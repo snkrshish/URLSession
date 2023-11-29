17cfaf9b5a71912e8e7eba6b7bf8de82bd77b63c
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2023-11-29 10:14:31 +0800

    wrote a default request

--- hw-19.playground/Contents.swift
+++ hw-19.playground/Contents.swift
@@ -1,4 +1,16 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func getData(urlRequest: String) {
 
+    let urlRequest = URL(string: urlRequest)
+    guard let url = urlRequest else { return }
+
+    URLSession.shared.dataTask(with: url) { data, response, error in
+        if error != nil {
+        } else if let response = response as? HTTPURLResponse, response.statusCode == 200 {
+            guard let data = data else { return }
+            let dataAsString = String(data: data, encoding: .utf8)
+        }
+    }.resume()
+}
+getData(urlRequest: "https://cat-fact.herokuapp.com/facts")
