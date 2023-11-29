9583a468258b14961251f59309166e10eccdd1ff
Nikita Shilyaev <92994263+snkrshish@users.noreply.github.com>
2023-11-29 10:30:02 +0800

    print Data

--- hw-19.playground/Contents.swift
+++ hw-19.playground/Contents.swift
@@ -3,7 +3,10 @@ import Foundation
 func getData(urlRequest: String) {
 
     let urlRequest = URL(string: urlRequest)
-    guard let url = urlRequest else { return }
+    guard let url = urlRequest else { 
+        print("Неверный URL")
+        return
+    }
 
     URLSession.shared.dataTask(with: url) { data, response, error in
         if error != nil {
@@ -13,6 +16,13 @@ func getData(urlRequest: String) {
         } else {
             print("Не получили ответ от сервера")
         }
+
+        if let data = data, let dataAsString = String(data: data, encoding: .utf8) {
+            print(" Данные с сервера: \(dataAsString)")
+        } else {
+            print("Данные не могут быть загружены или конвертированы в текстовый формат")
+        }
+
     }.resume()
 }
 getData(urlRequest: "https://cat-fact.herokuapp.com/facts")
